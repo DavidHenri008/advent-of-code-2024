407ee8748441856a2ab869fdf5cd2ed5f3b66b4b
David Henri <dxh06@acuitysso.com>
2024-12-06 16:44:52 -0500

    Update code :)

--- src/day 6/index.js
+++ src/day 6/index.js
@@ -67,6 +67,81 @@ const part1 = () => {
 };
 
 /**** Part TWO ****/
+// const part2 = () => {
+//   let result = 0;
+//   const map = parseData();
+//   const maxI = map.length;
+//   const maxJ = map[0].length;
+//   const [initI, initJ] = findGuard(map);
+
+//   // Loop through all map positions.
+//   for (let posI = 0; posI < map.length; posI++) {
+//     for (let posJ = 0; posJ < map[0].length; posJ++) {
+//       // Clone map.
+//       const newMap = [...map];
+//       // Skip if it is an obstacle '#' or a guard '^'.
+//       if (newMap[posI][posJ] === '#' || newMap[posI][posJ] === '^') {
+//         continue;
+//       }
+//       // Replace position by obstacle '#'.
+//       newMap[posI] = newMap[posI].substring(0, posJ) + '#' + newMap[posI].substring(posJ + 1);
+
+//       // Begin the guard movement.
+//       let sameSteps = 0;
+//       let continueLoop = true;
+//       let i = initI;
+//       let j = initJ;
+//       let offsetI = -1;
+//       let offsetJ = 0;
+//       while (continueLoop && sameSteps <= maxI * maxJ) {
+//         // Detect if it is a new position.
+//         if (newMap[i][j] !== 'X') {
+//           newMap[i] = newMap[i].substring(0, j) + 'X' + newMap[i].substring(j + 1);
+//           // Reset same position steps counter.
+//           sameSteps = 0;
+//         } else {
+//           // Count how many time the guard steps in the same position.
+//           sameSteps++;
+//         }
+//         // Calculate new guard position.
+//         const currentI = i;
+//         const currentJ = j;
+//         i += offsetI;
+//         j += offsetJ;
+//         // Detect if it is out of the map.
+//         if (i < 0 || i >= maxI || j < 0 || j >= maxJ) {
+//           continueLoop = false;
+//         } else if (newMap[i][j] === '#') {
+//           // Calculate new guard direction.
+//           if (offsetI === -1 && offsetJ === 0) {
+//             offsetI = 0;
+//             offsetJ = 1;
+//           } else if (offsetI === 0 && offsetJ === 1) {
+//             offsetI = 1;
+//             offsetJ = 0;
+//           } else if (offsetI === 1 && offsetJ === 0) {
+//             offsetI = 0;
+//             offsetJ = -1;
+//           } else if (offsetI === 0 && offsetJ === -1) {
+//             offsetI = -1;
+//             offsetJ = 0;
+//           }
+//           // Calculate new guard position once the direction is changed.
+//           i = currentI + offsetI;
+//           j = currentJ + offsetJ;
+//         }
+//       }
+
+//       // Detect if it is a valid path. Having true means that we were in a loop.
+//       if (continueLoop) {
+//         result++;
+//         //console.log(continueLoop, sameSteps, result);
+//       }
+//     }
+//   }
+
+//   console.log(result);
+// };
 const part2 = () => {
   let result = 0;
   const map = parseData();
@@ -94,24 +169,13 @@ const part2 = () => {
       let offsetI = -1;
       let offsetJ = 0;
       while (continueLoop && sameSteps <= maxI * maxJ) {
-        // Detect if it is a new position.
-        if (newMap[i][j] !== 'X') {
-          newMap[i] = newMap[i].substring(0, j) + 'X' + newMap[i].substring(j + 1);
-          // Reset same position steps counter.
-          sameSteps = 0;
-        } else {
-          // Count how many time the guard steps in the same position.
-          sameSteps++;
-        }
-        // Calculate new guard position.
-        const currentI = i;
-        const currentJ = j;
-        i += offsetI;
-        j += offsetJ;
         // Detect if it is out of the map.
         if (i < 0 || i >= maxI || j < 0 || j >= maxJ) {
           continueLoop = false;
         } else if (newMap[i][j] === '#') {
+          // Calculate previous guard position.
+          i -= offsetI;
+          j -= offsetJ;
           // Calculate new guard direction.
           if (offsetI === -1 && offsetJ === 0) {
             offsetI = 0;
@@ -126,9 +190,21 @@ const part2 = () => {
             offsetI = -1;
             offsetJ = 0;
           }
+          // Do not calculate new guard position here to avoid positionning over an obstacle.
+        } else {
+          // Detect if it is a new position.
+          if (newMap[i][j] !== 'X') {
+            newMap[i] = newMap[i].substring(0, j) + 'X' + newMap[i].substring(j + 1);
+            // Reset same position steps counter.
+            sameSteps = 0;
+          } else {
+            // Count how many time the guard steps in the same position.
+            sameSteps++;
+          }
+          // Only calculate new position here.
           // Calculate new guard position once the direction is changed.
-          i = currentI + offsetI;
-          j = currentJ + offsetJ;
+          i += offsetI;
+          j += offsetJ;
         }
       }
 
